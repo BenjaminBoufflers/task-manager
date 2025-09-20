6bdd5a74f49bbd13414621966ba06ad6c889d50f
Ben83000 <benjaminboufflers1@gmail.com>
2025-09-20 15:15:41 +0100

    update flake8 config

--- src/core/management/commands/wait_for_db.py
+++ src/core/management/commands/wait_for_db.py
@@ -1,5 +1,4 @@
 import time
-from unittest.mock import Base
 from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
