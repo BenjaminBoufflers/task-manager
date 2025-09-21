895986e044fad53b6dd0a878b78cfdbe9d68755d
Ben83000 <benjaminboufflers1@gmail.com>
2025-09-21 22:54:02 +0100

    reformat settings.py and wait_for_db with black

--- src/core/management/commands/wait_for_db.py
+++ src/core/management/commands/wait_for_db.py
@@ -7,14 +7,16 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database..')
+        self.stdout.write("Waiting for database..")
         for attempt in range(5):
             try:
-                self.check(databases=['default'])
-                self.stdout.write(self.style.SUCCESS('Database ready !'))
+                self.check(databases=["default"])
+                self.stdout.write(self.style.SUCCESS("Database ready !"))
                 return
             except (Psycopg2Error, OperationalError):
                 if attempt < 4:
-                    self.stderr.write('Database unvalaible, plase wait one second..')
+                    self.stderr.write("Database unvalaible, plase wait one second..")
                     time.sleep(1)
-        self.stderr.write(self.style.ERROR('Database unavalaible after 5 tries. Please retry later.'))
+        self.stderr.write(
+            self.style.ERROR("Database unavalaible after 5 tries. Please retry later.")
+        )
